aac303bb4de563c4755ba55ce1e48b699a12f7c1
Steve-Mac <s.hinkle1180@gmail.com>
2025-04-28 22:13:40 -0500

    Added splitting and bullet collision

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshape import CircleShape
 from constants import *
 
@@ -12,3 +13,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            randomAngle = random.uniform(20, 50)
+            new1 = self.velocity.rotate(randomAngle)
+            new2 = self.velocity.rotate(-randomAngle)
+            newRadius = self.radius - ASTEROID_MIN_RADIUS
+            a1 = Asteroid(self.position.x, self.position.y, newRadius)
+            a2 = Asteroid(self.position.x, self.position.y, newRadius)
+            a1.velocity = new1 * 1.2
+            a2.velocity = new2 * 1.2
--- main.py
+++ main.py
@@ -37,6 +37,12 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit() 
+        
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.split()
+                    shot.kill()
+
         screen.fill("black")
         for thing in drawable:
             thing.draw(screen)
